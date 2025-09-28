8bc0e0f63575d351d66723453b5653e538a7ef32
Saheli <engr.saheliroy@gmail.com>
2025-09-28 17:48:07 +0530

    Create Customer Slice with Redux Toolkit

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,3 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString()//side effect can be done in prepare method
+          }
+        }
+      },
+
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const {createCustomer, updateName} = customerSlice.actions;
+export default customerSlice.reducer;
+
+
+/*
+//Classic Reducer
+
 const initialStateCustomer = {
   fullName: "",
   nationalID: "",
@@ -34,3 +76,4 @@ export function createCustomer(fullName, nationalID) {
 export function updateName(fullName) {
   return { type: "customer/updateName", payload: fullName };
 }
+*/
